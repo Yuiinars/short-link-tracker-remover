4597ee321eafc08d0a150e7fae25d8fc38740bb7
Yuiinar <neko@hats-mail.com>
2024-09-13 15:22:55 +0900

    fix: xhsCleaner URI was escaped accidentally.

--- src/libs/cleaners/xhsCleaner.ts
+++ src/libs/cleaners/xhsCleaner.ts
@@ -2,25 +2,28 @@
 import { baseCleaner, CleanerResult } from "./baseCleaner";
 
 export function xhsCleaner(url: URL): CleanerResult {
-    const { url: baseCleanedUrl, debugInfo } = baseCleaner(url);
+    let { url: cleanedUrl, debugInfo } = baseCleaner(url);
 
     const allowedParams = ["xsec_token"];
     const newSearchParams = new URLSearchParams();
     const keptParams: string[] = [];
 
     for (const param of allowedParams) {
-        const value = baseCleanedUrl.searchParams.get(param);
+        const value = cleanedUrl.searchParams.get(param);
         if (value) {
             newSearchParams.set(param, value);
             keptParams.push(param);
         }
     }
 
-    baseCleanedUrl.search = newSearchParams.toString();
+    cleanedUrl.search = newSearchParams.toString();
 
     if (keptParams.length > 0) {
         debugInfo.push(`[Xiaohongshu Rules] Kept parameters: ${keptParams.join(", ")}`);
     }
 
-    return { url: baseCleanedUrl, debugInfo };
+    cleanedUrl.hash = "";
+    cleanedUrl = new URL(decodeURIComponent(cleanedUrl.toString()));
+
+    return { url: cleanedUrl, debugInfo };
 }
